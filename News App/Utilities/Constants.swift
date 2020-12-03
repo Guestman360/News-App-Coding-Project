1660af2c17acaac926dd06c97f649727fe080de3
Matthew Guest <matthewguest.dev@gmail.com>
2020-12-03 13:55:26 -0800

    First Commit, adding constants file

---
+++ News App/Utilities/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  News App
+//
+//  Created by Matthew Guest on 12/3/20.
+//
+
+import Foundation
+
+let BASE_API_URL = "https://s3.amazonaws.com/shrekendpoint/news.json"
