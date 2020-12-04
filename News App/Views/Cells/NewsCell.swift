9902c11029d344db7941bce529212b74ac17157d
Matthew Guest <matthewguest.dev@gmail.com>
2020-12-03 23:59:03 -0800

    Added two table view cells, one prominent for the first item and then a cell to implement a multi-item row. Still trying to decide how to implement multi-items in one row

---
+++ News App/Views/Cells/NewsCell.swift
@@ -0,0 +1,25 @@
+//
+//  NewsCell.swift
+//  News App
+//
+//  Created by Matthew Guest on 12/3/20.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+    
+    static let identifier = "NewsCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ News App/Views/Cells/ProminentNewsCell.swift
@@ -0,0 +1,25 @@
+//
+//  ProminentNewsCell.swift
+//  News App
+//
+//  Created by Matthew Guest on 12/3/20.
+//
+
+import UIKit
+
+class ProminentNewsCell: UITableViewCell {
+    
+    static let identifier = "ProminentNewsCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
